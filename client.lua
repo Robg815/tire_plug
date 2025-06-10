ceae2eae95ba76e03ab98de1ed487fcf8da29cd3
Smokey <137696662+Robg815@users.noreply.github.com>
2025-06-09 23:06:40 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,4 @@
-local SCRIPT_VERSION = '1.0.2'
-
-AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() == resourceName then
-        print(("[script:tire_plug] Script created by Smokey (client) - Version %s"):format(SCRIPT_VERSION))
-    end
-end)
+local SCRIPT_VERSION = '1.0.2'  -- your current script version
 
 local tireBones = {
     [0] = "wheel_lf",
@@ -13,49 +7,32 @@ local tireBones = {
     [5] = "wheel_rr"
 }
 
-local lockedTires = {}
-
-RegisterNetEvent('tire_plug:lockTire', function(vehicleNetId, tireIndex, state)
-    local key = ("%s_%s"):format(vehicleNetId, tireIndex)
-    lockedTires[key] = state
-end)
-
 RegisterNetEvent('tire_plug:attemptRepair', function(vehicle, tireIndex)
-    local netId = VehToNet(vehicle)
-    local key = ("%s_%s"):format(netId, tireIndex)
-
-    if lockedTires[key] then
-        lib.notify({ type = 'info', title = 'Busy', description = "This tire is currently being repaired by another player." })
-        return
-    end
-
-    local hasItem = exports.ox_inventory:Search('count', 'tire_plug') > 0
-    if not hasItem then
-        lib.notify({ type = 'error', title = 'Error', description = "You don't have a Tire Plug!" })
-        return
-    end
-
-    local passed = lib.skillCheck({'easy', 'medium'}, {'w', 'a', 's', 'd'})
-    if not passed then
-        lib.notify({ type = 'error', title = 'Failed', description = 'You failed the skillcheck!' })
-        return
-    end
-
     local ped = PlayerPedId()
+    local boneName = tireBones[tireIndex]
+    if not boneName then return end
 
-    -- Animation
+    local boneIndex = GetEntityBoneIndexByName(vehicle, boneName)
+    local boneCoords = GetWorldPositionOfEntityBone(vehicle, boneIndex)
+
+    -- Position player crouched slightly in front of the tire
+    local forwardVec = GetEntityForwardVector(vehicle)
+    local repairPos = boneCoords + forwardVec * 0.3
+    repairPos = vector3(repairPos.x, repairPos.y, repairPos.z - 0.85) -- crouched height adjustment
+
+    local vehCoords = GetEntityCoords(vehicle)
+    local headingToVehicle = GetHeadingFromVector_2d(vehCoords.x - repairPos.x, vehCoords.y - repairPos.y)
+
+    -- Teleport player to crouch position
+    SetEntityCoords(ped, repairPos.x, repairPos.y, repairPos.z, false, false, false, false)
+    SetEntityHeading(ped, headingToVehicle)
+    FreezeEntityPosition(ped, true)
+
+    -- Play crouched repair animation
     local dict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@"
     local anim = "machinic_loop_mechandplayer"
     RequestAnimDict(dict)
-    local timeout = GetGameTimer() + 5000
-    while not HasAnimDictLoaded(dict) do
-        if GetGameTimer() > timeout then
-            print("[tire_plug] Failed to load animation dictionary.")
-            return
-        end
-        Wait(10)
-    end
-
+    while not HasAnimDictLoaded(dict) do Wait(0) end
     TaskPlayAnim(ped, dict, anim, 8.0, -8.0, -1, 1, 0, false, false, false)
     PlaySoundFrontend(-1, "TOOLS", "MECHANIC", true)
 
@@ -66,34 +43,19 @@ RegisterNetEvent('tire_plug:attemptRepair', function(vehicle, tireIndex)
         disable = { move = true, car = true },
         onCancel = function()
             ClearPedTasks(ped)
+            FreezeEntityPosition(ped, false)
             lib.notify({ type = 'error', title = 'Cancelled', description = 'Repair was cancelled.' })
         end
     })
 
     ClearPedTasks(ped)
+    FreezeEntityPosition(ped, false)
 
     if success then
         local pedCoords = GetEntityCoords(ped)
+        local netId = VehToNet(vehicle)
         TriggerServerEvent('tire_plug:tryRepairTire', netId, tireIndex, pedCoords)
     end
-end)
-
-RegisterNetEvent('tire_plug:repairTire', function(vehicleNetId, tireIndex)
-    local vehicle = NetToVeh(vehicleNetId)
-    if not DoesEntityExist(vehicle) then return end
-
-    SetVehicleTyreFixed(vehicle, tireIndex)
-
-    if GetResourceState('xt-slashtires') == 'started' then
-        local success, err = pcall(function()
-            exports['xt-slashtires']:FixTire(vehicle, tireIndex)
-        end)
-        if not success then
-            print("[tire_plug] xt-slashtires error:", err)
-        end
-    end
-
-    lib.notify({ type = 'success', title = 'Success', description = 'Tire repaired.' })
 end)
 
 exports.ox_target:addGlobalVehicle({
@@ -105,7 +67,7 @@ exports.ox_target:addGlobalVehicle({
         canInteract = function(entity, distance)
             local netId = VehToNet(entity)
             local key = ("%s_%s"):format(netId, 0)
-            return not lockedTires[key] and IsVehicleTyreBurst(entity, 0, false)
+            return IsVehicleTyreBurst(entity, 0, false)
         end,
         onSelect = function(data)
             TriggerEvent('tire_plug:attemptRepair', data.entity, 0)
@@ -119,7 +81,7 @@ exports.ox_target:addGlobalVehicle({
         canInteract = function(entity, distance)
             local netId = VehToNet(entity)
             local key = ("%s_%s"):format(netId, 1)
-            return not lockedTires[key] and IsVehicleTyreBurst(entity, 1, false)
+            return IsVehicleTyreBurst(entity, 1, false)
         end,
         onSelect = function(data)
             TriggerEvent('tire_plug:attemptRepair', data.entity, 1)
@@ -133,7 +95,7 @@ exports.ox_target:addGlobalVehicle({
         canInteract = function(entity, distance)
             local netId = VehToNet(entity)
             local key = ("%s_%s"):format(netId, 4)
-            return not lockedTires[key] and IsVehicleTyreBurst(entity, 4, false)
+            return IsVehicleTyreBurst(entity, 4, false)
         end,
         onSelect = function(data)
             TriggerEvent('tire_plug:attemptRepair', data.entity, 4)
@@ -147,7 +109,7 @@ exports.ox_target:addGlobalVehicle({
         canInteract = function(entity, distance)
             local netId = VehToNet(entity)
             local key = ("%s_%s"):format(netId, 5)
-            return not lockedTires[key] and IsVehicleTyreBurst(entity, 5, false)
+            return IsVehicleTyreBurst(entity, 5, false)
         end,
         onSelect = function(data)
             TriggerEvent('tire_plug:attemptRepair', data.entity, 5)
